bdf5947045d6b4cb3444fe3090527be65b03dfe6
dks-customs <dominik.kaczmarczyk961@gmail.com>
2023-08-17 20:34:41 +0200

    removed double google ads tag

--- header.php
+++ header.php
@@ -47,8 +47,6 @@
         <link rel="canonical" href="<?php echo esc_attr( $canonical ); ?>">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
         <meta name="google-site-verification" content="kpkYwctDy2DB8b_Q_fvZ_flFJ0mSxlIaWaEwzak1R6U" />
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6315861678595518"
-     crossorigin="anonymous"></script>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta property="og:title" content="<?php echo esc_attr( $title ); ?>">
         <meta property="twitter:title" content="<?php echo esc_attr( $title ); ?>">
