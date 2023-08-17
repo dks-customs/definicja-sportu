d5c5c57b5ef4245b1543e8629be39f6eb255eee0
dks-customs <dominik.kaczmarczyk961@gmail.com>
2023-08-17 20:29:12 +0200

    transfer to calc

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,86 @@
-"use strict";var triggerSearchNode=document.getElementById("trigger-search"),searchForm=document.querySelector(".main-header__search__form");triggerSearchNode&&triggerSearchNode.addEventListener("click",function(){var e=searchForm.querySelector("input");e&&searchForm&&("flex"===searchForm.style.display?(searchForm.style.display="none",e.value=""):searchForm.style.display="flex")});var giveCookiesConsentBtn=document.getElementById("give-cookies-consent"),declineCookiesConsentBtn=document.getElementById("decline-cookies-consent"),cookiesConsent=document.getElementById("cookies-consent");cookiesConsent&&(giveCookiesConsentBtn.addEventListener("click",function(){function e(){dataLayer.push(arguments)}document.cookie="ds-cookies-consent=given;path=/;expires=Thu, 11 Feb 2297 13:54:13 GMT",window.dataLayer=window.dataLayer||[],e("js",new Date),e("config","G-4XTMS09KMT"),cookiesConsent.remove()}),declineCookiesConsentBtn.addEventListener("click",function(){document.cookie="ds-cookies-consent=declined;path=/;expires=Thu, 11 Feb 2297 13:54:13 GMT",cookiesConsent.remove()}));var listViewBtn=document.getElementById("list-view"),gridViewBtn=document.getElementById("grid-view"),archivePosts=document.getElementById("archive-posts");listViewBtn&&gridViewBtn&&(listViewBtn.addEventListener("click",function(){document.cookie="ds-view=list;path=/;expires=Thu, 11 Feb 2297 13:54:13 GMT",gridViewBtn.classList.remove("selected"),listViewBtn.classList.add("selected"),archivePosts&&(archivePosts.classList="posts-list")}),gridViewBtn.addEventListener("click",function(){document.cookie="ds-view=grid;path=/;expires=Thu, 11 Feb 2297 13:54:13 GMT",listViewBtn.classList.remove("selected"),gridViewBtn.classList.add("selected"),archivePosts&&(archivePosts.classList="posts-grid row gx-sm-4 gx-lg-5")}));var popHeader=document.getElementById("pop-header"),triggerPopSearch=document.getElementById("trigger-pop-search"),popSearchForm=document.getElementById("pop-header-search-form"),input=popSearchForm.querySelector("input");if(triggerPopSearch&&popSearchForm&&popHeader&&input){var e=function e(){var t=document.getElementById("wpadminbar"),s=window.scrollY;t&&popHeader.classList.add("admin-bar"),s>=500?popHeader.classList.add("show"):(popHeader.classList.remove("show"),popSearchForm.style.display="none",input.value="")};window.addEventListener("scroll",e),triggerPopSearch.addEventListener("click",function(){"flex"===popSearchForm.style.display?(popSearchForm.style.display="none",input.value=""):popSearchForm.style.display="flex"})}
+"use strict";
+
+var triggerSearchNode = document.getElementById("trigger-search");
+var searchForm = document.querySelector(".main-header__search__form");
+if (triggerSearchNode) {
+  triggerSearchNode.addEventListener("click", function () {
+    var input = searchForm.querySelector("input");
+    if (input && searchForm) {
+      if (searchForm.style.display === "flex") {
+        searchForm.style.display = "none";
+        input.value = "";
+      } else {
+        searchForm.style.display = "flex";
+      }
+    }
+  });
+}
+var giveCookiesConsentBtn = document.getElementById("give-cookies-consent");
+var declineCookiesConsentBtn = document.getElementById("decline-cookies-consent");
+var cookiesConsent = document.getElementById("cookies-consent");
+if (cookiesConsent) {
+  giveCookiesConsentBtn.addEventListener("click", function () {
+    document.cookie = "ds-cookies-consent=given;path=/;expires=Thu, 11 Feb 2297 13:54:13 GMT";
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", "G-4XTMS09KMT");
+    cookiesConsent.remove();
+  });
+  declineCookiesConsentBtn.addEventListener("click", function () {
+    document.cookie = "ds-cookies-consent=declined;path=/;expires=Thu, 11 Feb 2297 13:54:13 GMT";
+    cookiesConsent.remove();
+  });
+}
+var listViewBtn = document.getElementById("list-view");
+var gridViewBtn = document.getElementById("grid-view");
+var archivePosts = document.getElementById("archive-posts");
+if (listViewBtn && gridViewBtn) {
+  listViewBtn.addEventListener("click", function () {
+    document.cookie = "ds-view=list;path=/;expires=Thu, 11 Feb 2297 13:54:13 GMT";
+    gridViewBtn.classList.remove("selected");
+    listViewBtn.classList.add("selected");
+    if (archivePosts) {
+      archivePosts.classList = "posts-list";
+    }
+  });
+  gridViewBtn.addEventListener("click", function () {
+    document.cookie = "ds-view=grid;path=/;expires=Thu, 11 Feb 2297 13:54:13 GMT";
+    listViewBtn.classList.remove("selected");
+    gridViewBtn.classList.add("selected");
+    if (archivePosts) {
+      archivePosts.classList = "posts-grid row gx-sm-4 gx-lg-5";
+    }
+  });
+}
+var popHeader = document.getElementById("pop-header");
+var triggerPopSearch = document.getElementById("trigger-pop-search");
+var popSearchForm = document.getElementById("pop-header-search-form");
+var input = popSearchForm.querySelector("input");
+if (triggerPopSearch && popSearchForm && popHeader && input) {
+  var showPopHeader = function showPopHeader() {
+    var adminBar = document.getElementById("wpadminbar");
+    var y = window.scrollY;
+    if (adminBar) {
+      popHeader.classList.add("admin-bar");
+    }
+    if (y >= 500) {
+      popHeader.classList.add("show");
+    } else {
+      popHeader.classList.remove("show");
+      popSearchForm.style.display = "none";
+      input.value = "";
+    }
+  };
+  window.addEventListener("scroll", showPopHeader);
+  triggerPopSearch.addEventListener("click", function () {
+    if (popSearchForm.style.display === "flex") {
+      popSearchForm.style.display = "none";
+      input.value = "";
+    } else {
+      popSearchForm.style.display = "flex";
+    }
+  });
+}
